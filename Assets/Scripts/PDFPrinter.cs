a8b3e8b060798e4e729bab8fe01cb0d56ca69b4d
Matéo Avventuriero <77232541+Proutymat@users.noreply.github.com>
2025-06-11 01:37:29 +0200

    feat(printer): add new ticket length

--- Assets/Scripts/PDFPrinter.cs
+++ Assets/Scripts/PDFPrinter.cs
@@ -12,6 +12,7 @@ using System.Collections;
 public class PDFPrinter : MonoBehaviour
 {
     [SerializeField] private Camera camera;
+    [SerializeField] private string pdfFileNameDebug;
     
     private GameManager gameManager;
     private string printerName = "EPSON TM-T20 Receipt";
@@ -31,7 +32,7 @@ public class PDFPrinter : MonoBehaviour
         imageCounter = 0;
     }
 
-    private void CreatePDFWithImage(Texture2D texture)
+    private void CreatePDFWithImage(Texture2D texture, bool longTicket)
     {
         string imageName = Path.Combine(gameManager.LogFolderPath, $"map_{imageCounter}.png");
         imageCounter++;
@@ -50,7 +51,8 @@ public class PDFPrinter : MonoBehaviour
         PdfDocument document = new PdfDocument();
         PdfPage page = document.AddPage();
         page.Width = XUnit.FromCentimeter(8);
-        page.Height = XUnit.FromCentimeter(29.7);
+        
+        page.Height = XUnit.FromCentimeter(longTicket ? 327.6 : 29.7);
 
         XGraphics gfx = XGraphics.FromPdfPage(page);
         XImage image = XImage.FromFile(imageName);
@@ -86,26 +88,34 @@ public class PDFPrinter : MonoBehaviour
         return result;
     }
     
-    public void PrintPDF()
+    public void PrintPDF(string fileName)
     {
         var startInfo = new System.Diagnostics.ProcessStartInfo
         {
             FileName = sumatraPath,
-            Arguments = $"-print-to \"{printerName}\" -print-settings \"fit\" \"{pdfPath + pdfFileName}\"",
+            Arguments = $"-print-to \"{printerName}\" -print-settings \"fit,bin=B_BOTH,margins=small\" \"{pdfPath + fileName}\"",
             CreateNoWindow = true,
             UseShellExecute = false
         };
 
         System.Diagnostics.Process.Start(startInfo);
     }
-    
-    [Button]
-    public IEnumerator Print()
+
+
+    public IEnumerator Print(bool longTicket = false)
     {
         yield return new WaitForEndOfFrame();
+        const int size = 512;
+        Texture2D square = CaptureCameraSquare(camera, size);
+        CreatePDFWithImage(square, longTicket);
+        PrintPDF(pdfFileName);
+    }
+    
+    [Button]
+    public void PrintDebug()
+    {
         const int size = 512; // ou 1024 selon ta qualité
         Texture2D square = CaptureCameraSquare(camera, size);
-        CreatePDFWithImage(square);
-        PrintPDF();
+        PrintPDF(pdfFileNameDebug);
     }
 }
